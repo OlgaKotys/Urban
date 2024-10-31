bd6d49e19fc752158f6dfa6e722bbaba0b43b345
Olga_Kotys <olga.kotys@gmail.com>
2024-11-01 00:52:33 +0500

    finished

--- module_1_6.py
+++ module_1_6.py
@@ -1,16 +1,14 @@
 my_dict = {"Яблок" : 258, "Бананов" : 426, "Капусты" : 37}
 print(my_dict)
 print(my_dict["Бананов"])
-my_dict["Кукурузы"] = 45
+my_dict_1 = my_dict.get("Кукурузы")
+print(my_dict_1)
 my_dict.update({"Черешни" : 337,
                 "Картошки" : 585})
-del my_dict["Капусты"]
-print(my_dict.get("Капусты", "Такого продукта нет"))
 print(my_dict)
 
 my_set = {458, 326, 334, 'string', 458, 334, True, 326, 458, 'string'}
 print(my_set)
-my_set = set(my_set)
 my_set.add(300), my_set.add(False)
 my_set.discard('string')
 print(my_set)
