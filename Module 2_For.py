e020fb861611a344babc73ebde9ff2bb96952436
Olga_Kotys <olga.kotys@gmail.com>
2024-11-18 19:06:08 +0500

    finished all

--- Module 2_For.py
+++ Module 2_For.py
@@ -2,11 +2,19 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
 for n in numbers:
-    for d in range(1, n+1):
-        if n % d == 0 and d != 1 and n != 1 and n != d:
-            not_primes.append(n)
+    if n == 1:
+        continue
+
+    is_prime = True
+    for d in range(2, n):
+        if n % d == 0:
+            is_prime = False
             break
-    if n % d != 0 or n == d and n != 1:
+
+    if is_prime:
         primes.append(n)
+    else:
+        not_primes.append(n)
+
 print('Primes:', primes)
 print('Not primes:', not_primes)
