aea2c0d2e8cf1e440254213e0e64cbd5ff168fe9
Olga_Kotys <olga.kotys@gmail.com>
2024-12-13 19:31:08 +0500

    finished

--- module 7_5.py
+++ module 7_5.py
@@ -6,10 +6,10 @@ def scan_directory(directory):
         for file in files:
             filepath = os.path.join(root, file)
             filetime = os.path.getmtime(filepath)
-            format_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
+            formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
             filesize = os.path.getsize(filepath)
             parent_dir = os.path.dirname(filepath)
             print(
-                f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {format_time}, Родительская директория: {parent_dir}')
+                f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
 directory = 'D:\My Documents\Учёба\Урбан'
 scan_directory(directory)
